382e777dee720ad21146b2698686cb32a142c6f7
AbhijitKakhandiki <contactvega@gmail.com>
2013-07-25 17:59:16 +0000

    Updated web.js to read from index.html 12

--- web.js
+++ web.js
@@ -15,7 +15,7 @@ var data = fs.readFileSync('index.html');
 //  var in = fs.readFileSync('index.html').toString());
 //  response.send.(in);
   //response.send('Hello World 3 this time!');
-});
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
