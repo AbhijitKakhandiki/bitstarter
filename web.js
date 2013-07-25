d9d0844675ad351ed34dfc5e2e88101a8ce5fe2e
AbhijitKakhandiki <contactvega@gmail.com>
2013-07-25 17:26:17 +0000

    Updated web.js to read from index.html 5

--- web.js
+++ web.js
@@ -3,8 +3,9 @@ var app = express.createServer(express.logger());
 var infile = "index.html";
 
 app.get('/', function(request, response) {
-  var in = fs.readFileSync(infile);
-  response.end.(in.toString());
+  var in = fs.readFileSync(infile, 'utf8');
+  //response.send.(in.toString());
+  response.send('Hello World 3 this time!');
 });
 
 var port = process.env.PORT || 5000;
